e949bf6fed90335d1e893faa993125f342f2834e
Eric Boateng <eboateng85@gmail.com>
2016-03-02 02:59:49 -0500

    Add: MVC allocations + Favicon & logo img

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# deal_finder
+# Deal Finder!
+
 It's like that site you bought that deal from, used it, and then was like ohhh snap! Good deal! (Instructional Use Only)
--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Require framework & middleware
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,17 +6,18 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+// Route variables
+var routes = require('./app/routes/index');
+var users = require('./app/routes/users');
 
+// Initiate framework
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// View engine setup
+app.set('views', path.join(__dirname, 'app', 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'exclamation-mark-favicon.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,17 +27,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
+// Error Handlers
+// Catch 404 -> Error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
+// Development errors -> Will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -46,8 +46,7 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
+// Production errors -> No stacktraces to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
