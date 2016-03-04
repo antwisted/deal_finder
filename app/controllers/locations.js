4b2b997a6b9f0443639b8bbd5c316969f662194d
Eric Boateng <eboateng85@gmail.com>
2016-03-04 09:22:35 -0500

    Add: Data transfer from Views to Controllers - incomplete

--- app/controllers/locations.js
+++ app/controllers/locations.js
@@ -1,14 +1,111 @@
+var pry = require('pryjs');
+
 /* Main Page */
 module.exports.homeList = function (req, res){
-	res.render('directory', { title: 'Deal Finder!' }); 
+	res.render('directory', {
+		title: 'Deal Finder - Find a Local Deal Now!',
+		header: {
+			title: 'Deal Finder!',
+			tagline: 'Find a deal in your neighborhood.'
+		},
+		sidebar: "Looking for a brunch place? Enjoy free wifi and complimentary perks at these locations! Happy Hours from 3pm to 9pm.",
+		locations: [
+			{
+				name: "Ester's SmokeShack",
+				address: "125 High Street, New York, 10001",
+				rating: 3,
+				attributes: ["BBQ", "Beer specials", "Premium Sports HD"],
+				distance: "100m"
+			},
+			{
+				name: "Dan's Place",
+				address: "125 High Street, New York, 10001",
+				rating: 3,
+				attributes: ["Hot drinks", "Food", "Free wifi"],
+				distance: "300m"
+			},
+			{
+				name: "Jared's Jive",
+				address: "747 Fly Court, New York, 10001",
+				rating: 5,
+				attributes: ["Live Music", "Rooftop Bar", "2 Floors"],
+				distance: "550m"
+			},
+			{
+				name: "Marina's Marina",
+				address: "125 High Street, New York, 10001",
+				rating: 4,
+				attributes: ["Seafood", "Ocean View", "Prix Fixe"],
+				distance: "600m"
+			},
+			{
+				name: "Shawn's Dive Bar",
+				address: "999 Push Avenue",
+				rating: 4,
+				attributes: ["Pool Table", "Skee Ball", "$2 Drafts"],
+				distance: "700m"
+			}
+		]
+	});
+	// var testObj = {
+	// 	title: 'Deal Finder - Find a local deal!',
+	// 	header: {
+	// 		title: 'Deal Finder!',
+	// 		tagline: 'Find a deal in your neighborhood.'
+	// 	},
+	// 	locations: [
+	// 		{
+	// 			name: "Dan's Place",
+	// 			address: "125 High Street, New York, 10001",
+	// 			rating: 3,
+	// 			attributes: ["Hot drinks", "Food", "Free wifi"],
+	// 			distance: "300m"
+	// 		},
+	// 		{
+	// 			name: "Ester's SmokeShack",
+	// 			address: "125 High Street, New York, 10001",
+	// 			rating: 3,
+	// 			attributes: ["BBQ", "Beer specials", "Premium Sports HD"],
+	// 			distance: "100m"
+	// 		},
+	// 		{
+	// 			name: "Jared's Jive",
+	// 			address: "747 Fly Court, New York, 10001",
+	// 			rating: 5,
+	// 			attributes: ["Live Music", "Rooftop Bar", "2 Floors"],
+	// 			distance: "550m"
+	// 		},
+	// 		{
+	// 			name: "Marina's Marina",
+	// 			address: "125 High Street, New York, 10001",
+	// 			rating: 4,
+	// 			attributes: ["Seafood", "Ocean View", "Prix Fixe"],
+	// 			distance: "600m"
+	// 		},
+	// 		{
+	// 			name: "Shawn's Dive Bar",
+	// 			address: "999 Push Avenue",
+	// 			rating: 4,
+	// 			attributes: ["Pool Table", "Skee Ball", "$2 Drafts"],
+	// 			distance: "700m"
+	// 		}
+	// 	]
+	// };
+	// eval(pry.it);
 };
 
 /* Specific Location Page */
 module.exports.locationInfo = function (req, res){
-	res.render('locationInfo', { title: 'Location Info' }); 
+	res.render('locationInfo', {
+		title: 'Deal Finder - Location Info',
+		header: { title: "Dan's Place" }
+	}); 
 };
 
 /* Add New Review Page */
 module.exports.addReview = function (req, res){
-	res.render('reviewForm', { title: 'Add Review' }); 
+	res.render('reviewForm', {
+		title: 'Deal Finder - Add a Review',
+		header: { title: "Review Dan's Place" }
+	}); 
 };
--- app/controllers/misc.js
+++ app/controllers/misc.js
@@ -1,4 +1,10 @@
 /* About page */
 module.exports.about = function (req, res){
-	res.render('about', { title: 'About' }); 
+	res.render('about', { 
+		title: 'About Deal Finder...',
+		page: {
+			title: "About Us",
+			content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+		}
+	}); 
 };
